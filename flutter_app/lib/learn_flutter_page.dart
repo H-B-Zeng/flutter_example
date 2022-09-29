1fb5fe7243bb2caaaf799350d58039a430d18ce1
SC4014 <Ben.Tseng@fusheng.com>
2022-09-29 14:12:50 +0800

    Add ListView<widget> sample

--- flutter_app/lib/learn_flutter_page.dart
+++ flutter_app/lib/learn_flutter_page.dart
@@ -8,6 +8,8 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwithch = false;
+  bool? isCheckbox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,11 +22,99 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back),
         ),
-      ),
-      body: Column(
-        children: [
-          Image.asset('images/air-compressor.png'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('actions');
+            },
+            icon: Icon(Icons.info_outline),
+          ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/air-compressor.png'),
+            SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'This is text widget',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwithch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint('ElevatedButton');
+              },
+              child: Text('ElevatedButton'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('OutlinedButton');
+              },
+              child: Text('OutlinedButton'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('TextdButton');
+              },
+              child: Text('OutlinedButton'),
+            ),
+            GestureDetector(
+              behavior:
+                  HitTestBehavior.opaque, //Click row will be trigger ontap
+              onTap: () {
+                debugPrint('This is the row');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text('Row widget'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwithch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwithch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckbox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckbox = newBool;
+                  });
+                }),
+            Image.network(
+                'https://s3.amazonaws.com/external_clips/attachments/2734108/original/AdobeStock_271374055.jpg')
+          ],
+        ),
       ),
     );
   }
